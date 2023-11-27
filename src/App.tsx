e0d7b085f31878ad40582f05f4a9564a08aef5e5
xiesuzhu <xiesuzhu@anhuai.com>
2023-11-27 21:04:35 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/app.css";
+import './styles/output.css';
+
+function App() {
+  return (
+    <div className="bg-gray-100">
+      <h1 className="text-2xl font-bold text-blue-600">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
